1bdbacdf9e71d7dc83d38d8f229a69a992b56517
Robin Gower <robin.git@infonomics.ltd.uk>
2018-02-20 12:09:20 +0100

    CSV serialization

--- table2qb/src/table2qb/core.clj
+++ table2qb/src/table2qb/core.clj
@@ -1,49 +1,95 @@
 (ns table2qb.core
   (:require [clojure.data.csv :as csv]
-            [net.cgrand.xforms :as x]))
+            [net.cgrand.xforms :as x]
+            [clojure.java.io :as io]))
 
-(def standardise-header
-  {"GeographyCode" :geography
-   "DateCode" :date
-   "Measurement" :measure
-   "Units" :unit
-   "Value" :value
-   "SITC Section" :sitc-section
-   "Flow" :flow})
+;; CSV handling
 
-(defn read-csv [reader]
-  (let [csv-data (csv/read-csv reader)]
-    (map zipmap
-         (->> (first csv-data)
-              (map standardise-header)
-              repeat)
-         (rest csv-data))))
+(defn read-csv
+  ([reader]
+   "Reads converting headers to keywords"
+   (read-csv reader keyword))
+  ([reader header-mapping]
+   "Reads csv into seq of hashes, with mapping from headers to keys"
+   (let [csv-data (csv/read-csv reader)]
+     (map zipmap
+          (->> (first csv-data)
+               (map header-mapping)
+               repeat)
+          (rest csv-data)))))
+
+(defn unkeyword [keyword]
+  "Converts a keyword in to a string without the leading colon"
+  (subs (str keyword) 1))
+
+(defn write-csv [writer data]
+  (csv/write-csv writer
+                 (cons (map unkeyword (keys (first data))) (map vals data))))
+
+
+;; Conventions
+
+(defn title->name [title]
+  "Standardises a title to a unambiguious internal name"
+  ({"GeographyCode" :geography
+     "DateCode" :date
+     "Measurement" :measure
+     "Units" :unit
+     "Value" :value
+     "SITC Section" :sitc_section
+     "Flow" :flow} title (keyword title)))
+
+;; Component attributes for an name
+(def name->component
+  (with-open [rdr (-> "components.csv" io/resource io/reader)]
+    (let [component-attributes (read-csv rdr)]
+      (zipmap (map (comp keyword :component_slug) component-attributes)
+              component-attributes))))
+
+
+
+
+;; Identifying Components
 
 (defn headers-matching [pred]
   (comp (take 1) (map keys) cat (filter pred)))
 
-(def is-dimension? #{:geography :date :sitc-section :flow})
+(def is-dimension? #{:geography :date :sitc_section :flow})
 (def is-attribute? #{:unit})
 
 (defn append-measures-dimension [xf]
   (fn
     ([] (xf))
-    ([result] (xf (xf result) :measure-type))
+    ([result] (xf (xf result) :measure_type))
     ([result input] (xf result input))))
 
 (def dimensions
   (comp (headers-matching is-dimension?)
-        append-measures-dimension))
+        append-measures-dimension
+        (map name->component)))
 
-(def attributes (headers-matching is-attribute?))
+(def attributes
+  (comp (headers-matching is-attribute?)
+        (map name->component)))
 
-(def standardise-measure {"Value" :value, "Net Mass", :net-mass})
+(def standardise-measure {"Value" :value, "Net Mass", :net_mass})
 
 (def measures
-  (comp (map :measure) (distinct) (map standardise-measure)))
+  (comp (map :measure)
+        (distinct)
+        (map standardise-measure) ;; replace with title->name?
+        (map name->component)))
 
 (defn components
-  "Takes an filename for csv of observations and returns a components dataset"
+  "Takes an filename for csv of observations and returns a sequence of components"
   [reader]
-  (let [data (read-csv reader)]
+  (let [data (read-csv reader title->name)]
     (sequence (x/multiplex [dimensions attributes measures]) data)))
+
+(comment
+  (let [reader xxx
+        components (components reader)
+        csv-writer xxx
+        json-writer xxx]
+    (write-csv csv-writer (csv components))
+    (write-json json-writer (json components))))
--- table2qb/test/table2qb/core_test.clj
+++ table2qb/test/table2qb/core_test.clj
@@ -1,7 +1,8 @@
 (ns table2qb.core-test
   (:require [clojure.test :refer :all]
             [table2qb.core :refer :all]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io])
+  (:import [java.io StringWriter]))
 
 (defn example [filename]
   (str "./test/resources/trade-example/" filename))
@@ -11,4 +12,20 @@
     (with-open [input-reader (io/reader (example "input.csv"))]
       (let [components (doall (components input-reader))]
         (testing "one row per component"
-          (is (= 8 (-> components count))))))))
+          (is (= 8 (-> components count))))
+        (testing "geography component"
+          (let [{:keys [:component_attachment :component_property]}
+                (first (filter #(= (:component_slug %) "geography") components))]
+            (is (= component_attachment "qb:dimension"))
+            (is (= component_property "http://purl.org/linked-data/sdmx/2009/dimension#refArea"))))
+        (testing "compare with components.csv"
+          (testing "parsed contents match"
+            (with-open [target-reader (io/reader (example "components.csv"))]
+              (is (= (set (read-csv target-reader))
+                     (set components)))))
+          (testing "serialised contents match"
+            (with-open [target-reader (io/reader (example "components.csv"))]
+              (let [string-writer (StringWriter.)]
+                (write-csv string-writer (sort-by :component_slug components))
+                (is (= (slurp target-reader)
+                       (str string-writer)))))))))))

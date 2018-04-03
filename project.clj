58825d467ca973008d64183691a786ab9719d2e9
Robin Gower <robin.git@infonomics.ltd.uk>
2018-04-03 17:59:07 +0200

    Bump version to 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject table2qb "0.1.0-SNAPSHOT"
+(defproject table2qb "0.1.1-SNAPSHOT"
   :description "Transform tables of observations and reference data into RDF data cube resources specified as csvw"
   :url "http://publishmydata.com"
   :license {:name "Eclipse Public License"

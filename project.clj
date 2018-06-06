e6c7c15f6faefccd574636ce5af3df04ae4684a7
lkitching <lee@swirrl.com>
2018-06-06 17:21:43 +0100

    Update dependent version of csv2rdf.
    Update csv2rdf to version 0.2.3. lazy-concat has been renamed to
    liberal-concat, and its signature has been changed to match that
    of concat.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject table2qb "0.1.2-SNAPSHOT"
+(defproject table2qb "0.1.3-SNAPSHOT"
   :description "Transform tables of observations and reference data into RDF data cube resources specified as csvw"
   :url "http://publishmydata.com"
   :license {:name "Eclipse Public License"
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [grafter/grafter "0.11.2"]
                  [grafter/extra "0.2.2-grafter-0.11.2-SNAPSHOT"]
-                 [csv2rdf "0.2.2-SNAPSHOT"]
+                 [csv2rdf "0.2.3"]
                  [org.clojure/data.csv "0.1.4"]
                  [net.cgrand/xforms "0.16.0"]
                  [environ "1.1.0"]]

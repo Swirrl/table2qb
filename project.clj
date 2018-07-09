e6dc5d3bd563d0ce6bf7a05f758056dfebec027d
lkitching <lee@swirrl.com>
2018-07-09 11:15:10 +0100

    Add integrant dependency.
    Add integrant and use the config to declare available pipelines and
    their arguments.

--- project.clj
+++ project.clj
@@ -9,6 +9,7 @@
                  [csv2rdf "0.2.4"]
                  [org.clojure/data.csv "0.1.4"]
                  [net.cgrand/xforms "0.16.0"]
-                 [environ "1.1.0"]]
+                 [environ "1.1.0"]
+                 [integrant "0.6.3"]]
   :plugins [[lein-environ "1.1.0"]]
   :profiles {:dev {:env {:base-uri "http://gss-data.org.uk/"}}})

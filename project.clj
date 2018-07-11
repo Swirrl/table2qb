feb95528aaa6760b01da5228248484cc72b39cb2
lkitching <lee@swirrl.com>
2018-07-11 22:29:47 +0100

    Version 0.3.0-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject table2qb "0.2.0"
+(defproject table2qb "0.3.0-SNAPSHOT"
   :description "Transform tables of observations and reference data into RDF data cube resources specified as csvw"
   :url "http://publishmydata.com"
   :license {:name "Eclipse Public License"

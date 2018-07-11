dc0fe05bb4bbc39e09ccf5c5d704bf426fd9dfbd
lkitching <lee@swirrl.com>
2018-07-11 14:26:37 +0100

    Create travis build.
    Issue #46 - Create travis build which runs the tests, builds an
    uberjar and packages it within a zip file. Tag builds also create
    a github release.

---
+++ travis/profiles.clj
@@ -0,0 +1,14 @@
+{:system
+  {:repositories
+  [["swirrl-jars-snapshots"
+    {:url "s3p://swirrl-jars/snapshots/"
+     :username :env
+     :passphrase :env
+     :releases false}]
+   ["swirrl-jars-releases"
+    {:url "s3p://swirrl-jars/releases/"
+     :username :env
+     :passphrase :env
+     :snapshots false}]]}
+ :user
+ {:plugins [[s3-wagon-private "1.1.2" :exclusions [commons-logging commons-codec]]]}}

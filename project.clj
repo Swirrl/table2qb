17781e6707645ee40a31ff7a5b7deeaf2ef58220
lkitching <lee@swirrl.com>
2018-07-12 16:29:12 +0100

    Version 0.1.3.
    Use version 0.2.2 of grafter-extra and bump version to 0.1.3.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject table2qb "0.1.3-SNAPSHOT"
+(defproject table2qb "0.1.3"
   :description "Transform tables of observations and reference data into RDF data cube resources specified as csvw"
   :url "http://publishmydata.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [grafter/grafter "0.11.2"]
-                 [grafter/extra "0.2.2-grafter-0.11.2-SNAPSHOT"]
+                 [grafter/extra "0.2.2"]
                  [csv2rdf "0.2.4"]
                  [org.clojure/data.csv "0.1.4"]
                  [net.cgrand/xforms "0.16.0"]

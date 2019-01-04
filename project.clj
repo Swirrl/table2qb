93f3514bf834f1edb6df61454458c2c256daecf1
lkitching <lee@swirrl.com>
2019-01-04 10:22:37 +0000

    Update version of lein-tools-deps.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://publishmydata.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-tools-deps "0.4.1"]]
+  :plugins [[lein-tools-deps "0.4.3"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
   :lein-tools-deps/config {:config-files [:install :user :project]}
   :dependencies [[org.clojure/clojure "1.9.0"]

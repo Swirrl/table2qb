dfb8a14d0d925394399abdacee05186dacbd4b63
Robin Gower <robin.git@infonomics.ltd.uk>
2019-01-09 18:43:26 +0100

    Set travis to deploy to swirrl maven repo

---
+++ travis/profiles.clj
@@ -0,0 +1,13 @@
+{:system
+ {:repositories
+  [["snapshots"
+    {:url "s3p://swirrl-jars/snapshots/"
+     :username :env
+     :passphrase :env
+     :releases false}]
+   ["releases"
+    {:url "s3p://swirrl-jars/releases/"
+     :username :env
+     :passphrase :env
+     :snapshots false}]]
+  :plugins [[s3-wagon-private "1.3.1"]]}}

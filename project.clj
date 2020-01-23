e45bc364c4256b18d597891f56894c64529ed81f
Rick Moynihan <rick.m@swirrl.com>
2020-01-23 14:19:53 +0000

    Bump lein-tools-deps version to 0.4.5

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://publishmydata.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-tools-deps "0.4.3"]]
+  :plugins [[lein-tools-deps "0.4.5"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
   :lein-tools-deps/config {:config-files [:install :user :project]}
   :profiles {:uberjar {:main table2qb.main

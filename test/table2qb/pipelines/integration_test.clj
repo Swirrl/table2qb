5066d23a35b31a542678adfacb240a0eece961a6
Rick Moynihan <rick.m@swirrl.com>
2020-06-01 15:51:51 +0100

    Fix indentation

--- test/table2qb/pipelines/integration_test.clj
+++ test/table2qb/pipelines/integration_test.clj
@@ -14,63 +14,63 @@
   (testing "Validates table2qb outputs against pmd, dataset, and cube tests"
     (testing "Overseas Trade"
       (with-repository [repo (repo/fixture-repo
-                               ;; Third party vocabularies
-                               (io/resource "vocabularies/sdmx-dimension.ttl")
-                               (io/resource "vocabularies/qb.ttl")
-                               (io/resource "overseas-trade/vocabularies/2012.rdf")
-                               (io/resource "overseas-trade/vocabularies/CN_2015_20180206_105537.ttl"))]
-                       (with-open [conn (repo/->connection repo)]
-                         (add-csvw conn codelist/codelist-pipeline {:codelist-csv (io/resource "regional-trade/csv/flow-directions.csv")
-                                                       :codelist-name             "Flow Directions"
-                                                       :codelist-slug             "flow-directions"
-                                                       :base-uri                  test-domain})
-                         (add-csvw conn codelist/codelist-pipeline {:codelist-csv (io/resource "regional-trade/csv/sitc-sections.csv")
-                                                       :codelist-name             "SITC Sections"
-                                                       :codelist-slug             "sitc-sections"
-                                                       :base-uri                  test-domain})
-                         (add-csvw conn codelist/codelist-pipeline {:codelist-csv (io/resource "regional-trade/csv/units.csv")
-                                                       :codelist-name             "Measurement Units"
-                                                       :codelist-slug             "measurement-units"
-                                                       :base-uri                  test-domain})
-                         (add-csvw conn components/components-pipeline {:input-csv (io/resource "regional-trade/csv/components.csv")
+                              ;; Third party vocabularies
+                              (io/resource "vocabularies/sdmx-dimension.ttl")
+                              (io/resource "vocabularies/qb.ttl")
+                              (io/resource "overseas-trade/vocabularies/2012.rdf")
+                              (io/resource "overseas-trade/vocabularies/CN_2015_20180206_105537.ttl"))]
+        (with-open [conn (repo/->connection repo)]
+          (add-csvw conn codelist/codelist-pipeline {:codelist-csv (io/resource "regional-trade/csv/flow-directions.csv")
+                                                     :codelist-name             "Flow Directions"
+                                                     :codelist-slug             "flow-directions"
+                                                     :base-uri                  test-domain})
+          (add-csvw conn codelist/codelist-pipeline {:codelist-csv (io/resource "regional-trade/csv/sitc-sections.csv")
+                                                     :codelist-name             "SITC Sections"
+                                                     :codelist-slug             "sitc-sections"
+                                                     :base-uri                  test-domain})
+          (add-csvw conn codelist/codelist-pipeline {:codelist-csv (io/resource "regional-trade/csv/units.csv")
+                                                     :codelist-name             "Measurement Units"
+                                                     :codelist-slug             "measurement-units"
+                                                     :base-uri                  test-domain})
+          (add-csvw conn components/components-pipeline {:input-csv (io/resource "regional-trade/csv/components.csv")
                                                          :base-uri                 test-domain})
 
-                         ;;this dataset
-                         (add-csvw conn codelist/codelist-pipeline {:codelist-csv (io/resource "overseas-trade/csv/countries.csv")
-                                                       :codelist-name             "Countries"
-                                                       :codelist-slug             "countries"
-                                                       :base-uri                  test-domain})
-                         (add-csvw conn components/components-pipeline {:input-csv (io/resource "overseas-trade/csv/components.csv")
+          ;;this dataset
+          (add-csvw conn codelist/codelist-pipeline {:codelist-csv (io/resource "overseas-trade/csv/countries.csv")
+                                                     :codelist-name             "Countries"
+                                                     :codelist-slug             "countries"
+                                                     :base-uri                  test-domain})
+          (add-csvw conn components/components-pipeline {:input-csv (io/resource "overseas-trade/csv/components.csv")
                                                          :base-uri                 test-domain})
-                         (add-csvw conn cube/cube-pipeline {:input-csv (io/resource "overseas-trade/csv/ots-cn-sample.csv")
-                                                   :dataset-name       "Overseas Trade Sample"
-                                                   :dataset-slug       "overseas-trade-sample"
-                                                   :column-config      default-config
-                                                   :base-uri           test-domain}))
+          (add-csvw conn cube/cube-pipeline {:input-csv (io/resource "overseas-trade/csv/ots-cn-sample.csv")
+                                             :dataset-name       "Overseas Trade Sample"
+                                             :dataset-slug       "overseas-trade-sample"
+                                             :column-config      default-config
+                                             :base-uri           test-domain}))
 
-                       (testing "PMD Validation"
-                         (is (empty? (pmd/errors repo))))
-                       (testing "PMD Dataset Validation"
-                         (is (empty? (remove #{"is missing a reference area dimension"
-                                               "is not a pmd:Dataset"
-                                               "is missing a pmd:graph"}
-                                             (pmdd/errors repo (str test-domain-data "overseas-trade-sample"))))))
-                       (testing "Sort Priority"
-                         (with-open [conn (repo/->connection repo)]
-                           (let [results (query conn (slurp (io/resource "validation/sparql/sort-priority.sparql")))
-                                 schemes (->> results (map (comp str :scheme)) distinct)]
-                             (testing "may be provided"
-                               (is (some #{"http://gss-data.org.uk/def/concept-scheme/sitc-sections"} schemes)))
-                             (testing "is optional"
-                               (is (not-any? #{"http://gss-data.org.uk/def/concept-scheme/flow-directions"} schemes))))))
-                       (testing "Description"
-                         (with-open [conn (repo/->connection repo)]
-                           (let [results (query conn (slurp (io/resource "validation/sparql/description.sparql")))
-                                 schemes (->> results (map (comp str :scheme)) distinct)]
-                             (testing "may be provided"
-                               (is (some #{"http://gss-data.org.uk/def/concept-scheme/sitc-sections"} schemes)))
-                             (testing "is optional"
-                               (is (not-any? #{"http://gss-data.org.uk/def/concept-scheme/flow-directions"} schemes))))))))))
+        (testing "PMD Validation"
+          (is (empty? (pmd/errors repo))))
+        (testing "PMD Dataset Validation"
+          (is (empty? (remove #{"is missing a reference area dimension"
+                                "is not a pmd:Dataset"
+                                "is missing a pmd:graph"}
+                              (pmdd/errors repo (str test-domain-data "overseas-trade-sample"))))))
+        (testing "Sort Priority"
+          (with-open [conn (repo/->connection repo)]
+            (let [results (query conn (slurp (io/resource "validation/sparql/sort-priority.sparql")))
+                  schemes (->> results (map (comp str :scheme)) distinct)]
+              (testing "may be provided"
+                (is (some #{"http://gss-data.org.uk/def/concept-scheme/sitc-sections"} schemes)))
+              (testing "is optional"
+                (is (not-any? #{"http://gss-data.org.uk/def/concept-scheme/flow-directions"} schemes))))))
+        (testing "Description"
+          (with-open [conn (repo/->connection repo)]
+            (let [results (query conn (slurp (io/resource "validation/sparql/description.sparql")))
+                  schemes (->> results (map (comp str :scheme)) distinct)]
+              (testing "may be provided"
+                (is (some #{"http://gss-data.org.uk/def/concept-scheme/sitc-sections"} schemes)))
+              (testing "is optional"
+                (is (not-any? #{"http://gss-data.org.uk/def/concept-scheme/flow-directions"} schemes))))))))))
 
 
 ;; TODO: Vocabulary for pmd:usedCode
